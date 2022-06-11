e11e1c75e4b6ad96f38134b8c4a0d7d48562b5c8
Mathias <mathias.pedersen834@gmail.com>
2022-06-11 22:39:19 +0200

    asd

--- lua51-lifter/src/chunk/function/mod.rs
+++ lua51-lifter/src/chunk/function/mod.rs
@@ -3,20 +3,18 @@ use nom::{combinator::*, multi::*, number::complete::*, *};
 use crate::{
     chunk::function::{
         debug::{local::*, position::*, upvalue},
-        ellipsis::*,
     },
     instruction::*,
     value::*,
 };
 
 mod debug;
-mod ellipsis;
 
 #[derive(Debug)]
 pub struct Function<'a> {
     pub line_defined: u32,
     pub last_line_defined: u32,
-    pub ellipsis_flag: Flag,
+    pub ellipsis_flag: u8,
     pub maximum_stack_size: u8,
     pub code: Vec<Instruction>,
     pub constants: Vec<ValueId<'a>>,
@@ -28,11 +26,13 @@ pub struct Function<'a> {
 
 impl<'a> Function<'a> {
     pub fn parse(input: &'a [u8]) -> IResult<&'a [u8], Self> {
+        println!("test");
+        let (input, _) = parse_str(input)?;
         let (input, line_defined) = le_u32(input)?;
         let (input, last_line_defined) = le_u32(input)?;
         let (input, upvalues_length) = le_u8(input)?;
         let (input, parameters_length) = le_u8(input)?;
-        let (input, ellipsis_flag) = ellipsis::Flag::parse(input)?;
+        let (input, ellipsis_flag) = le_u8(input)?;
         let (input, maximum_stack_size) = le_u8(input)?;
         let (input, code_length) = le_u32(input)?;
         let (input, code) = count(Instruction::parse, code_length as usize)(input)?;
--- lua51-lifter/src/chunk/mod.rs
+++ lua51-lifter/src/chunk/mod.rs
@@ -11,21 +11,17 @@ pub mod header;
 #[derive(Debug)]
 pub struct Chunk<'a> {
     pub header: Header,
-    pub source_name: &'a str,
     pub main: Function<'a>,
 }
 
 pub(crate) fn parse(input: &[u8]) -> IResult<&[u8], Chunk> {
     let (input, header) = Header::parse(input)?;
-    let (input, source_name) = parse_str(input)?;
-    println!("{}", source_name);
     let (input, main) = Function::parse(input)?;
 
     Ok((
         input,
         Chunk {
             header,
-            source_name,
             main,
         },
     ))
