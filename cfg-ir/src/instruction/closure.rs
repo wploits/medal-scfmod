280de4d38446623b69b7697de3356e58d64fa2fe
Jujhar Singh <jujharsmc@gmail.com>
2022-06-17 10:15:53 +1200

    add closure to cfg ir

---
+++ cfg-ir/src/instruction/closure.rs
@@ -0,0 +1,36 @@
+use std::{fmt, rc::Rc};
+
+use crate::{value::ValueId, function::Function};
+
+use super::value_info::ValueInfo;
+
+
+#[derive(Debug, Clone)]
+pub struct Closure {
+    pub dest: ValueId,
+    pub function: Rc<Function>,
+}
+
+impl ValueInfo for Closure {
+    fn values_read(&self) -> Vec<ValueId> {
+        vec![]
+    }
+
+    fn values_written(&self) -> Vec<ValueId> {
+        vec![self.dest]
+    }
+
+    fn values_read_mut(&mut self) -> Vec<&mut ValueId> {
+        vec![]
+    }
+
+    fn values_written_mut(&mut self) -> Vec<&mut ValueId> {
+        vec![&mut self.dest]
+    }
+}
+
+impl fmt::Display for Closure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} <- Function", self.dest)
+    }
+}
