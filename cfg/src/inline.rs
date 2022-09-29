af24315af7be4e43abecefcf76e2ffb57b3c2925
Jujhar Singh <jujharsmc@gmail.com>
2022-09-29 17:01:34 +1300

    inlining go vroom

--- cfg/src/inline.rs
+++ cfg/src/inline.rs
@@ -4,17 +4,17 @@ use fxhash::{FxHashMap, FxHashSet};
 use indexmap::IndexMap;
 use itertools::Either;
 
-fn assigns(
-    block: &ast::Block,
+fn assigns<'a>(
+    block: &'a ast::Block,
     locals_out: &FxHashSet<&ast::RcLocal>,
     check_statement: usize,
     ref_local: &ast::RcLocal,
-) -> Option<ast::RValue> {
+) -> Option<&'a ast::RValue> {
     if let ast::Statement::Assign(assign) = &block[check_statement] {
         if assign.left.len() == 1 && assign.right.len() == 1 {
             if let ast::LValue::Local(local) = &assign.left[0] {
                 if local == ref_local && !locals_out.contains(local) {
-                    return Some(assign.right[0].clone());
+                    return Some(&assign.right[0]);
                 }
             }
         }
@@ -27,10 +27,11 @@ fn inline_expression(
     read: &ast::RcLocal,
     new_expression: ast::RValue,
 ) {
+    let mut new_expression = Some(new_expression);
     statement.post_traverse_values(&mut |v| {
         if let Either::Right(rvalue) = v {
             if let ast::RValue::Local(rvalue_local) = rvalue && *rvalue_local == *read {
-                *rvalue = new_expression.clone();
+                *rvalue = new_expression.take().unwrap();
                 return Some(())
             }
         }
@@ -69,37 +70,45 @@ fn inline_expressions(function: &mut Function, upvalue_to_group: &IndexMap<ast::
                     .flat_map(|e| e.arguments.iter().map(|(_, a)| a)),
             );
         }
+
+        // TODO: rename values_read to locals_read
+        let mut stat_to_values_read = Vec::new();
+        for stat in &block.ast.0 {
+            stat_to_values_read.push(
+                stat.values_read()
+                    .into_iter()
+                    .filter(|&l| local_usages[l] == 1 && !upvalue_to_group.contains_key(l))
+                    .cloned()
+                    .collect::<Vec<_>>(),
+            );
+        }
+
         let mut index = 0;
         'w: while index < block.ast.len() {
             for stat_index in (0..index).rev() {
-                for read in block.ast[index]
-                    .values_read()
-                    .into_iter()
-                    .cloned()
-                    .collect::<Vec<_>>()
-                {
-                    if local_usages[&read] > 1 || upvalue_to_group.contains_key(&read) {
-                        continue;
-                    }
-                    if let Some(new_expression) =
-                        assigns(&block.ast, &locals_out, stat_index, &read)
+                for read in &stat_to_values_read[index] {
+                    if let ast::Statement::Assign(assign) = &block.ast[stat_index]
+                        && assign.left.len() == 1
+                        && assign.right.len() == 1
+                        && let ast::LValue::Local(local) = &assign.left[0]
+                        && local == read
+                        && !locals_out.contains(local)
                     {
-                        let statement = &mut block.ast[index];
-                        if !new_expression.has_side_effects() {
-                            inline_expression(statement, &read, new_expression);
-                            block.ast[stat_index] = ast::Empty {}.into();
-                            continue 'w;
-                        }
-                        if let Some(res) = statement.rvalues_mut().into_iter().find_map(|rvalue| {
-                            if rvalue.values_read().contains(&&read) {
+                        let new_expression = &assign.right[0];
+                        if !new_expression.has_side_effects() || block.ast[index].rvalues().into_iter().find_map(|rvalue| {
+                            if rvalue.values_read().contains(&read) {
                                 Some(true)
                             } else if rvalue.has_side_effects() {
                                 Some(false)
                             } else {
                                 None
                             }
-                        }) && res {
-                            inline_expression(statement, &read, new_expression);
+                        }).unwrap_or(false) {
+                            let new_expression = std::mem::replace(
+                                &mut block.ast[stat_index],
+                                ast::Empty {}.into())
+                                .into_assign().unwrap().right.into_iter().next().unwrap();
+                            inline_expression(&mut block.ast[index], read, new_expression);
                             block.ast[stat_index] = ast::Empty {}.into();
                             continue 'w;
                         }
