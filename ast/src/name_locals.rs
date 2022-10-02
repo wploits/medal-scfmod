718a2138c0a2fbd56d8dc5c76d04271219a960b0
Jujhar Singh <jujharsmc@gmail.com>
2022-10-02 13:46:32 +1300

    fix local naming for params, for-loops

--- ast/src/name_locals.rs
+++ ast/src/name_locals.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 
-use crate::{Block, RValue, Statement, Traverse};
+use crate::{Block, RValue, Statement, Traverse, RcLocal};
 
 struct Namer {
     rename: bool,
@@ -8,11 +8,26 @@ struct Namer {
 }
 
 impl Namer {
+    fn name_local(&mut self, local: &RcLocal) {
+        if self.rename || local.0 .0.borrow().0.is_none() {
+            // TODO: hacky
+            if Rc::strong_count(local) == 1 {
+                local.0 .0.borrow_mut().0 = Some("_".to_string());
+            } else {
+                local.0 .0.borrow_mut().0 = Some(format!("l_{}", self.counter));
+                self.counter += 1;
+            }
+        }
+    }
+
     fn name_locals(&mut self, block: &mut Block) {
         for statement in &mut block.0 {
             // TODO: traverse_values
             statement.post_traverse_values(&mut |value| -> Option<()> {
                 if let itertools::Either::Right(RValue::Closure(closure)) = value {
+                    for param in &closure.parameters {
+                        self.name_local(param);
+                    }
                     self.name_locals(&mut closure.body);
                 };
                 None
@@ -20,16 +35,7 @@ impl Namer {
             match statement {
                 Statement::Assign(assign) if assign.prefix => {
                     for lvalue in &assign.left {
-                        let local = lvalue.as_local().unwrap();
-                        if self.rename || local.0 .0.borrow().0.is_none() {
-                            // TODO: hacky and doesnt work for parameters
-                            if Rc::strong_count(local) == 1 {
-                                local.0 .0.borrow_mut().0 = Some("_".to_string());
-                            } else {
-                                local.0 .0.borrow_mut().0 = Some(format!("l_{}", self.counter));
-                                self.counter += 1;
-                            }
-                        }
+                        self.name_local(lvalue.as_local().unwrap());
                     }
                 }
                 Statement::If(r#if) => {
@@ -45,9 +51,13 @@ impl Namer {
                     self.name_locals(&mut r#while.block);
                 }
                 Statement::NumericFor(numeric_for) => {
+                    self.name_local(&numeric_for.counter);
                     self.name_locals(&mut numeric_for.block);
                 }
                 Statement::GenericFor(generic_for) => {
+                    for res_local in &generic_for.res_locals {
+                        self.name_local(res_local);
+                    }
                     self.name_locals(&mut generic_for.block);
                 }
                 _ => {}
