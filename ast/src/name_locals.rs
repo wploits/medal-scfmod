2277a0c39472fc28cc40b2a69ab6878239dc48f7
wploits!!! <166282207+wploits@users.noreply.github.com>
2025-09-19 00:25:09 +0900

    Update name_locals.rs

--- ast/src/name_locals.rs
+++ ast/src/name_locals.rs
@@ -1,37 +1,122 @@
-use rustc_hash::FxHashSet;
+use rustc_hash::{FxHashMap, FxHashSet};
 use triomphe::Arc;
+use itertools::Itertools;
 
-use crate::{Block, RValue, RcLocal, Statement, Traverse, Upvalue};
+use crate::{Block, RValue, RcLocal, Statement, Traverse, Upvalue, Call, MethodCall, Literal, Index, Assign};
 
 struct Namer {
     rename: bool,
     counter: usize,
     upvalues: FxHashSet<RcLocal>,
+    name_hints: FxHashMap<RcLocal, String>,
 }
 
 impl Namer {
     fn name_local(&mut self, prefix: &str, local: &RcLocal) {
         let mut lock = local.0 .0.lock();
         if self.rename || lock.0.is_none() {
-            // TODO: hacky and slow
-            if Arc::count(&local.0 .0) == 1 {
+            if let Some(hint) = self.name_hints.get(local) {
+                let name_prefix = if self.upvalues.contains(local) { "_u_" } else { "" };
+                lock.0 = Some(format!("{}{}", name_prefix, hint));
+            }
+            else if Arc::count(&local.0 .0) == 1 {
                 lock.0 = Some("_".to_string());
             } else {
-                let prefix = prefix.to_string()
-                    + if self.upvalues.contains(local) {
-                        "_u_"
-                    } else {
-                        ""
-                    };
-                lock.0 = Some(format!("{}{}_", prefix, self.counter));
+                let name_prefix = if self.upvalues.contains(local) { "_u_" } else { "" };
+                lock.0 = Some(format!("{}{}{}", prefix, name_prefix, self.counter));
                 self.counter += 1;
             }
         }
     }
 
+    fn collect_name_hints(&mut self, block: &mut Block) {
+        for statement in &mut block.0 {
+            if let Statement::Assign(assign) = statement {
+                if assign.left.len() == 1 && assign.right.len() == 1 {
+                    if let Some(lvalue_local) = assign.left[0].as_local() {
+                        let rvalue = &assign.right[0];
+                        if let Some(hint) = self.get_hint_from_rvalue(rvalue) {
+                            self.name_hints.insert(lvalue_local.clone(), hint);
+                        }
+                    }
+                }
+            }
+            
+            // Add hint for table containing RemoteEvent/RemoteFunction
+            if let Statement::GenericFor(generic_for) = statement {
+                if generic_for.res_locals.len() == 2 {
+                    if let Some(local_v2) = generic_for.right.get(0).and_then(|r| r.as_local()) {
+                        if self.name_hints.get(local_v2) == Some(&"remotes".to_string()) {
+                            if let Some(local_v4) = generic_for.res_locals.get(1) {
+                                self.name_hints.insert(local_v4.clone(), "remote".to_string());
+                            }
+                        }
+                    }
+                }
+            }
+
+            match statement {
+                Statement::If(r#if) => {
+                    self.collect_name_hints(&mut r#if.then_block.lock());
+                    self.collect_name_hints(&mut r#if.else_block.lock());
+                },
+                Statement::While(r#while) => self.collect_name_hints(&mut r#while.block.lock()),
+                Statement::Repeat(repeat) => self.collect_name_hints(&mut repeat.block.lock()),
+                Statement::NumericFor(numeric_for) => self.collect_name_hints(&mut numeric_for.block.lock()),
+                Statement::GenericFor(generic_for) => self.collect_name_hints(&mut generic_for.block.lock()),
+                _ => {},
+            }
+        }
+    }
+
+    fn get_hint_from_rvalue(&self, rvalue: &RValue) -> Option<String> {
+        match rvalue {
+            RValue::Index(index) => match &*index.right {
+                RValue::Literal(Literal::String(string)) => {
+                    if let Ok(s) = std::str::from_utf8(string) {
+                        Some(s.to_string())
+                    } else {
+                        None
+                    }
+                }
+                _ => None,
+            },
+            RValue::Global(global) => {
+                if let Ok(s) = std::str::from_utf8(&global.0) {
+                    Some(s.to_string())
+                } else {
+                    None
+                }
+            }
+            RValue::Call(Call { value, arguments: _ }) => {
+                if let Some(MethodCall { value: _, method, arguments: args }) = value.as_method_call() {
+                    if method == "GetService" && args.len() == 1 {
+                        if let Some(RValue::Literal(Literal::String(arg_str))) = args.get(0) {
+                            if let Ok(s) = std::str::from_utf8(arg_str) {
+                                return Some(s.to_string());
+                            }
+                        }
+                    }
+                }
+                None
+            }
+            RValue::MethodCall(MethodCall { value: _, method, arguments }) => {
+                if method == "GetService" && arguments.len() == 1 {
+                    if let Some(RValue::Literal(Literal::String(arg_str))) = arguments.get(0) {
+                        if let Ok(s) = std::str::from_utf8(arg_str) {
+                            return Some(s.to_string());
+                        }
+                    }
+                }
+                None
+            }
+            RValue::Table(_) => Some("remotes".to_string()),
+            _ => None,
+        }
+    }
+
     fn name_locals(&mut self, block: &mut Block) {
         for statement in &mut block.0 {
-            // TODO: traverse_rvalues
             statement.post_traverse_values(&mut |value| -> Option<()> {
                 if let itertools::Either::Right(RValue::Closure(closure)) = value {
                     let mut function = closure.function.lock();
@@ -73,11 +158,8 @@ impl Namer {
         }
     }
 
-    // TODO: does this need to be mut?
     fn find_upvalues(&mut self, block: &mut Block) {
         for statement in &mut block.0 {
-            // TODO: traverse_values
-            // TODO: doesnt need to be mut
             statement.post_traverse_values(&mut |value| -> Option<()> {
                 if let itertools::Either::Right(RValue::Closure(closure)) = value {
                     self.upvalues.extend(
@@ -121,7 +203,9 @@ pub fn name_locals(block: &mut Block, rename: bool) {
         rename,
         counter: 1,
         upvalues: FxHashSet::default(),
+        name_hints: FxHashMap::default(),
     };
     namer.find_upvalues(block);
+    namer.collect_name_hints(block);
     namer.name_locals(block);
 }
